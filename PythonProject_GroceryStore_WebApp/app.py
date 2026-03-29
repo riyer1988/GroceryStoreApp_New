31d878940b2464189a19409db823df7dffb6796f
riyer1988 <riyer1988@gmail.com>
2026-03-29 21:38:22 +0530

    removal of all files
    removal of all files

--- PythonProject_GroceryStore_WebApp/app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Grocery Store", layout="wide")
-
-st.title("🛒 Grocery Store Management System")
-
-st.image("homepage.jpg", use_column_width=True)
-
-st.markdown("Use the sidebar to navigate between Orders and Products.")
